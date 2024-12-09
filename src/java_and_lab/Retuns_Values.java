4a29be52b155615f6ab86787dace2f6c414acb16
Aldo Wiranata <awiranata263@gmail.com>
2024-12-09 12:47:34 +0700

    add : class return values

---
+++ src/java_and_lab/Retuns_Values.java
@@ -0,0 +1,15 @@
+package java_and_lab;
+
+
+public class Retuns_Values {
+
+    static int numbers(int x , int y){
+        return x + y;
+        
+    }
+    public static void main(String[] args) {
+       int z = numbers(1, 2);
+       System.out.println(z);
+    }
+    
+}
