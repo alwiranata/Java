3a053f59577e565a457f31e7e29b4fabbbc166a4
Aldo Wiranata <awiranata263@gmail.com>
2024-12-19 14:11:46 +0700

    add : Variabel

---
+++ src/real_project/Var.java
@@ -0,0 +1,18 @@
+package real_project;
+public class Var {
+    public static void main(String[] args) {
+        // Tipe Data
+        int number = 10;              // Bilangan bulat
+        double price = 19.99;         // Bilangan desimal
+        char letter = 'A';            // Karakter tunggal
+        String text = "Hello, Java!"; // Teks
+        boolean isActive = true;      // True atau False
+
+        // Cetak ke konsol
+        System.out.println("Number: " + number);
+        System.out.println("Price: " + price);
+        System.out.println("Letter: " + letter);
+        System.out.println("Text: " + text);
+        System.out.println("Is Active: " + isActive);
+    }
+}
