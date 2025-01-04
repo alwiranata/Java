e30756472a2956ce229a71ab10bcd4a64f295813
Aldo Wiranata <148690939+alwiranata@users.noreply.github.com>
2025-01-04 21:11:58 +0700

    add : tugasKelompok2

---
+++ src/pakAqil/tugasKelompok2.java
@@ -0,0 +1,13 @@
+
+package pakAqil;
+
+import java.io.PrintWriter;
+public class tugasKelompok2 {
+    public static void main(String[] args){
+        PrintWriter writer = new PrintWriter(System.out , true);
+        
+        double  nilai = 100;
+        writer.printf("Cetak Menggunakan printf : %.2f\n", nilai);
+    }
+}
+
