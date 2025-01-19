af6043e84552e358036fa9ea79e52c6b6c75e43f
Aldo Wiranata <148690939+alwiranata@users.noreply.github.com>
2025-01-19 16:31:45 +0700

    update: GanjilGenap.java

--- src/pakAqil/GanjilGenap.java
+++ src/pakAqil/GanjilGenap.java
@@ -5,7 +5,10 @@ import javax.swing.JOptionPane;
 public class GanjilGenap {
     public static void main(String[] args){
         
-         //pilih opsi
+        
+        
+        try{
+           //pilih opsi
           String add = JOptionPane.showInputDialog(
                     "Pilih salah satu opsi:\n" +
                     "1. Untuk memeriksa satu angka (genap/ganjil)\n" +
@@ -13,32 +16,23 @@ public class GanjilGenap {
             );
           
             int opsi = Integer.parseInt(add);
-        
-        try{
-          
            
             if(opsi == 1 ){
                //input 1 angka
                String input = JOptionPane.showInputDialog("Masukkan angka");
                //mengubah input menjadi angka
                int angka =  Integer.parseInt(input);
-               
+            
                if(angka %2 == 0){
                    JOptionPane.showMessageDialog(null, angka + " Adalah Bilangan genap");
                }
                
-               else if(angka % 2 == 1){
+               if(angka % 2 == 1){
                   JOptionPane. showMessageDialog(null, angka + " Adalah Bilagan ganjil") ;
                }
-               
-               else{
-                   JOptionPane.showMessageDialog(null, angka + "Masukkan");
-               }
-               
-               
+               return;
            }
-            
-            else if(opsi == 2 ){
+            if(opsi == 2 ){
                 //input banyak angka
                 String input = JOptionPane.showInputDialog("Masukkan Banyak angka");
                 //mengubah input menjadi ankga
@@ -50,24 +44,20 @@ public class GanjilGenap {
                     JOptionPane.showMessageDialog(null, i +" Adalah Bilgan Genap");
                   }
                   //Ganjil
-                  else if(i % 2 == 1){
+                  if(i % 2 == 1){
                     JOptionPane.showMessageDialog(null, i + " Adalah Bilangan Ganjil" );
                   }
-                  
-                  else{
-                      JOptionPane.showMessageDialog(null, i + "pilihan tidak valid!");
-                  }
-                  
                 }
+                return;
             }
             
             else{
-                JOptionPane.showMessageDialog(null, "Pilihan tidak valid !");
+                JOptionPane.showMessageDialog(null, "Pilihan tidak tersedia!");
             }
             
         }catch(NumberFormatException e){ 
           JOptionPane.showMessageDialog(null, 
-                    "Input tidak valid! Harap masukkan angka yang benar.");
+                    "Terjadi Kesalahan " + e.getMessage());
         }
         
         
